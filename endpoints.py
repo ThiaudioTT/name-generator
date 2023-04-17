27ec810d2058d6ba2bffa502b5879c679dcef19a
ThiaudioTT <thiago.sky25@gmail.com>
2023-04-17 13:56:25 -0300

    feat: adds main program

---
+++ endpoints.py
@@ -0,0 +1,7 @@
+_list = [
+    'https://gerador-nomes.wolan.net/nome/aleatorio',
+    'https://gerador-nomes.wolan.net/nomes/1',
+    'https://gerador-nomes.wolan.net/apelidos/1',
+]
+
+length = len(_list)
---
+++ main.py
@@ -0,0 +1,22 @@
+import requests
+import random
+
+import endpoints
+
+# Generates a random name based on get requests from the internet
+def generateName():
+    iChooseAPI = random.randint(0, endpoints.length - 1)
+    response = requests.get(endpoints._list[iChooseAPI])
+
+    if response.status_code == 200:
+        return response.json()[0]
+    else:
+        print('Error: ' + str(response.status_code))
+        return 'Error'
+
+
+def main():
+    print('Generating name...')
+    print(generateName() + ' ' + generateName())
+
+main()
